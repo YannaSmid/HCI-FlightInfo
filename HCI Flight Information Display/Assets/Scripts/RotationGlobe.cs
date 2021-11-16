689e5fa9f18a49eb71351cb4747b91b9e76b2fae
YannaSmid <91134353+YannaSmid@users.noreply.github.com>
2021-11-16 22:33:41 +0100

    ff een opzetje voor de visuals

---
+++ HCI Flight Information Display/Assets/Scripts/RotationGlobe.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationGlobe : MonoBehaviour
+{
+    public Transform Globe;
+
+    public float speed = 1;
+    Vector3 DefaultRotation = new Vector3(0,0,0);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Globe.Rotate(0, 1f * Time.deltaTime * speed,0);
+    }
+}
