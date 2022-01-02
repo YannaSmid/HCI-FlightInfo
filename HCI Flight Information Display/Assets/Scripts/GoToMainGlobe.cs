d6b3130fd98701071fe7b36b6dcf765ddd017cfe
YannaSmid <91134353+YannaSmid@users.noreply.github.com>
2022-01-02 21:30:34 +0100

    added menu scene and instruction scene

---
+++ HCI Flight Information Display/Assets/Scripts/GoToMainGlobe.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToMainGlobe : MonoBehaviour
+{
+    public float pointtime = 20f;
+    private float countdown;
+    private bool isPointed = false;
+
+    public GameObject StartText;
+
+    void Update(){
+        
+        if (isPointed && countdown >= 0f){
+            countdown -= 1f;
+        }
+        else if (isPointed && countdown <= 0f){
+            SceneManager.LoadScene("MainGlobe");
+        }
+
+    }
+    
+    public void StartMainScene(){
+
+        isPointed = true;
+        countdown = pointtime;
+        StartText.SetActive(true);
+        
+    }
+
+    public void CutOffStart(){
+
+        isPointed = false;
+        StartText.SetActive(false);
+    }
+}
---
+++ HCI Flight Information Display/Assets/Scripts/RotationGlobe1.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationGlobe1 : MonoBehaviour
+{
+    bool startProgram = true;
+    //Handmodels
+    // public GameObject LeftHand;
+    // Transform HandPalm;
+    // Vector3 HandposNew;
+    // Vector3 HandposOld;
+    // Vector3 HandposDiff;
+    // float HandposDiffx;
+
+    //earth
+    public Transform Globe;
+    public bool CanRotate = false;
+    public float speed = 1;
+    Vector3 DefaultRotation = new Vector3(0,0,0);
+
+    // Start is called before the first frame update
+    void Start(){
+       
+       
+    }
+
+    public void Update(){
+        
+        // if (startProgram)
+        // {
+        //     // HandPalm = LeftHand.transform.GetChild(0);
+        //     // //Debug.Log(HandPalm.name);
+        //     // HandposNew = HandPalm.position;
+        //     // startProgram = false;
+        // }
+
+        // HandposOld = HandposNew;
+        // Debug.Log(HandposOld + "en" + HandposNew);
+        // HandposNew = HandPalm.position;
+        // if (CanRotate){
+        //     HandposDiff = HandposOld - HandposOld;
+        //     HandposDiffx = HandposNew.x * 100f;
+            //Debug.Log(HandposDiffx);
+        Globe.Rotate(0, 1f * Time.deltaTime * speed,0);
+        // }
+    }
+    // public void StartRotation(){
+    //     CanRotate = true;
+    // }
+
+    // public void StopRotation(){
+    //     CanRotate = false;
+    // }
+}
