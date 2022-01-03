08386ceed59893cadb40bc3f3608e3bc80a7cb82
YannaSmid <91134353+YannaSmid@users.noreply.github.com>
2022-01-03 15:06:37 +0100

    added instruction animations

---
+++ HCI Flight Information Display/Assets/Scripts/FromInsToMain.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FromInsToMain : MonoBehaviour
+{
+    public float pointtime = 30f;
+    private float countdown;
+    private bool isPointed = false;
+
+    public SpriteRenderer Icon;
+    public Color SelectedColor;
+    public Color DefaultColor;
+
+
+    void Start(){
+        DefaultColor = Icon.color;
+    }
+    void Update(){
+        
+        if (isPointed && countdown >= 0f){
+            countdown -= 1f;
+        }
+        else if (isPointed && countdown <= 0f){
+            SceneManager.LoadScene("MainGlobe");
+        }
+
+    }
+    
+    public void StartMenu(){
+
+        isPointed = true;
+        countdown = pointtime;
+        Icon.color = SelectedColor;
+        
+    }
+
+    public void CutOffMenu(){
+
+        isPointed = false;
+        Icon.color = DefaultColor;
+
+    }
+}
